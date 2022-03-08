337d705ae5798c0d278421641a63607961415b89
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-03-08 08:25:00 +0100

    Correction de Q28_4.java (double cast DANS le scalb).

--- src/ch/epfl/javelo/Q28_4.java
+++ src/ch/epfl/javelo/Q28_4.java
@@ -10,10 +10,10 @@ public final class Q28_4 {
     }
 
     public static double asDouble(int q28_4) {
-        return Math.scalb(q28_4, -4);
+        return Math.scalb((double) q28_4, -4);
     }
 
     public static float asFloat(int q28_4) {
-        return Math.scalb(q28_4, -4);
+        return Math.scalb((float) q28_4, -4);
     }
 }
