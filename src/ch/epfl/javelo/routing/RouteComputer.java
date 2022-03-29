e20bf789ec8169b98c3b975a0578a38e87d7a679
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-03-29 18:33:13 +0200

    Etape 6 ist fertig mein elsazische Freund.

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -4,7 +4,10 @@ import ch.epfl.javelo.Preconditions;
 import ch.epfl.javelo.data.Graph;
 import ch.epfl.javelo.projection.PointCh;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.function.DoubleUnaryOperator;
 
 /**
@@ -37,56 +40,35 @@ public final class RouteComputer {
     public Route bestRouteBetween(int startNodeId, int endNodeId) {
         Preconditions.checkArgument(startNodeId != endNodeId);
 
-        Map<Integer, Double> distances = new TreeMap<>();
+        List<Float> distances = new ArrayList<>();
         PriorityQueue<WeightedNode> exploring = new PriorityQueue<>();
-        Map<Integer, Integer> predecessors = new TreeMap<>();
+        List<Integer> predecessors = new ArrayList<>();
 
         for (int nodeId = 0; nodeId < graph.nodeCount(); nodeId++) {
-            distances.put(nodeId, Double.POSITIVE_INFINITY);
-            predecessors.put(nodeId, 0);
+            distances.add(Float.POSITIVE_INFINITY);
+            predecessors.add(0);
         }
-        distances.put(0, 0d);
-        exploring.add(new WeightedNode(startNodeId, 0d));
+        distances.set(0, 0f);
+        exploring.add(new WeightedNode(startNodeId, 0f));
 
         while (exploring.size() != 0) {
             WeightedNode node = exploring.remove();
-            distances.put(node.nodeId, Double.NEGATIVE_INFINITY);
-
+            distances.set(node.nodeId, Float.NEGATIVE_INFINITY);
             if (node.nodeId == endNodeId) {
-                List<Integer> nodes = new ArrayList<>();
-                int nodeId = endNodeId;
-                while (nodeId != startNodeId) {
-                    int predecessor = predecessors.get(nodeId);
-                    nodes.add(predecessor);
-                    nodeId = predecessor;
-                }
-                Collections.reverse(nodes);
-                List<Edge> edges = new ArrayList<>();
-                for (int i = 0; i < nodes.size() - 1; i++) {
-                    int fromNodeId = nodes.get(i);
-                    int toNodeId = nodes.get(i + 1);
-                    PointCh fromPoint = graph.nodePoint(fromNodeId);
-                    PointCh toPoint = graph.nodePoint(toNodeId);
-                    int edgeId = 0;
-                    for (int j = 0; j < graph.nodeOutDegree(fromNodeId); j++) {
-                        edgeId = graph.nodeOutEdgeId(fromNodeId, j);
-                        if (graph.edgeTargetNodeId(edgeId) == toNodeId)
-                            break;
-                    }
-                    double length = graph.edgeLength(edgeId);
-                    DoubleUnaryOperator profile = graph.edgeProfile(edgeId);
-                    edges.add(new Edge(fromNodeId, toNodeId, fromPoint, toPoint, length, profile));
-                }
+                List<Integer> routeNodes = getRouteNodes(predecessors, endNodeId, startNodeId);
+                List<Edge> edges = getRouteEdges(routeNodes);
                 return new SingleRoute(edges);
             }
-
-            for (int edgeIndex = 0; edgeIndex < graph.nodeOutDegree(node.nodeId); edgeIndex++) {
+            int nbOfEdges = graph.nodeOutDegree(node.nodeId);
+            for (int edgeIndex = 0; edgeIndex < nbOfEdges; edgeIndex++) {
                 int edgeId = graph.nodeOutEdgeId(node.nodeId, edgeIndex);
                 int arrivalNodeId = graph.edgeTargetNodeId(edgeId);
-                double distance = node.distance + this.costFunction.costFactor(node.nodeId, edgeId) * graph.edgeLength(edgeId);
+                float distance = (float) (node.distance
+                        + this.costFunction.costFactor(node.nodeId, edgeId)
+                        * graph.edgeLength(edgeId));
                 if (distance < distances.get(arrivalNodeId)) {
-                    distances.put(arrivalNodeId, distance);
-                    predecessors.put(arrivalNodeId, node.nodeId);
+                    distances.set(arrivalNodeId, distance);
+                    predecessors.set(arrivalNodeId, node.nodeId);
                     exploring.add(new WeightedNode(arrivalNodeId, distance));
                 }
             }
@@ -94,10 +76,42 @@ public final class RouteComputer {
         return null;
     }
 
-    record WeightedNode(int nodeId, double distance) implements Comparable<WeightedNode> {
+    private List<Integer> getRouteNodes(List<Integer> predecessors, int endNodeId, int startNodeId) {
+        List<Integer> routeNodes = new ArrayList<>();
+        int nodeId = endNodeId;
+        while (nodeId != startNodeId) {
+            int predecessor = predecessors.get(nodeId);
+            routeNodes.add(predecessor);
+            nodeId = predecessor;
+        }
+        Collections.reverse(routeNodes);
+        return routeNodes;
+    }
+
+    private List<Edge> getRouteEdges(List<Integer> routeNodes) {
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < routeNodes.size() - 1; i++) {
+            int fromNodeId = routeNodes.get(i);
+            int toNodeId = routeNodes.get(i + 1);
+            PointCh fromPoint = graph.nodePoint(fromNodeId);
+            PointCh toPoint = graph.nodePoint(toNodeId);
+            int edgeId = 0;
+            for (int j = 0; j < graph.nodeOutDegree(fromNodeId); j++) {
+                edgeId = graph.nodeOutEdgeId(fromNodeId, j);
+                if (graph.edgeTargetNodeId(edgeId) == toNodeId)
+                    break;
+            }
+            double length = graph.edgeLength(edgeId);
+            DoubleUnaryOperator profile = graph.edgeProfile(edgeId);
+            edges.add(new Edge(fromNodeId, toNodeId, fromPoint, toPoint, length, profile));
+        }
+        return edges;
+    }
+
+    record WeightedNode(int nodeId, float distance) implements Comparable<WeightedNode> {
         @Override
         public int compareTo(WeightedNode that) {
-            return Double.compare(this.distance, that.distance);
+            return Float.compare(this.distance, that.distance);
         }
     }
 }
