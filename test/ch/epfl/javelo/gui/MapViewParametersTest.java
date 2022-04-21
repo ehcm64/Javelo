dd61454e618e3784fadeba22166b6dc5e2fb7235
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-04-21 20:18:49 +0200

    Etape 7 finie et testée.

--- test/ch/epfl/javelo/gui/MapViewParametersTest.java
+++ test/ch/epfl/javelo/gui/MapViewParametersTest.java
@@ -9,11 +9,13 @@ class MapViewParametersTest {
 
     @Test
     void testXYReversal() {
-        double x = 4.2;
-        double y = 4.6;
-        MapViewParameters mvp = new MapViewParameters(4, x, y);
-        PointWebMercator pwm = mvp.pointAt(3, 4);
-        assertEquals(3, mvp.viewX(pwm));
-        assertEquals(4, mvp.viewY(pwm));
+        double xTopLeft = 4.2;
+        double yTopLeft = 4.6;
+        double xCoord = 3;
+        double yCoord = 4;
+        MapViewParameters mvp = new MapViewParameters(4, xTopLeft, yTopLeft);
+        PointWebMercator pwm = mvp.pointAt(xCoord, yCoord);
+        assertEquals(xCoord, mvp.viewX(pwm));
+        assertEquals(yCoord, mvp.viewY(pwm));
     }
 }
