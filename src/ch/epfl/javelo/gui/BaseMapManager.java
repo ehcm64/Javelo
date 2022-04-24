4e149257278ae3cda377fcc98ed2a3b717358ec1
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-04-24 21:15:23 +0200

    Bugs mais déplacement + zoom possibles

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,9 +1,12 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.Math2;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -15,6 +18,7 @@ public final class BaseMapManager {
     private boolean redrawNeeded;
     private Canvas canvas;
     private Pane pane;
+    private ObjectProperty<MouseEvent> mouse;
 
     public BaseMapManager(TileManager tm,
                           WaypointsManager wpm,
@@ -26,6 +30,7 @@ public final class BaseMapManager {
         this.pane = new Pane();
         this.canvas = new Canvas();
         this.redrawNeeded = true;
+        this.mouse = new SimpleObjectProperty<>();
 
         pane.getChildren().add(canvas);
         canvas.widthProperty().bind(pane.widthProperty());
@@ -35,6 +40,10 @@ public final class BaseMapManager {
             assert oldS == null;
             newS.addPreLayoutPulseListener(this::redrawIfNeeded);
         });
+
+        addHandlers();
+
+
     }
 
     public Pane pane() {
@@ -60,7 +69,6 @@ public final class BaseMapManager {
         for (int x = xTopLeftTileId; x <= xBottomRightTileId; x++) {
             for (int y = yTopLeftTileId; y <= yBottomRightTileId; y++) {
                 TileManager.TileId tileId = new TileManager.TileId(zl, x, y);
-
                 try {
                     gc.drawImage(tm.imageForTileAt(tileId),
                             x * 256 - xTopLeft,
@@ -76,5 +84,34 @@ public final class BaseMapManager {
     private void redrawOnNextPulse() {
         redrawNeeded = true;
         Platform.requestNextPulse();
+    }
+
+    private void addHandlers() {
+
+        pane.setOnMousePressed(e -> mouse.set(e));
+
+        pane.setOnMouseDragged(e -> {
+
+            mvp.set(mvp.get().withMinXY(mvp.get().xTopLeft() - (e.getX() - mouse.get().getX()),
+                    mvp.get().yTopLeft() - (e.getY() - mouse.get().getY())));
+            mouse.set(e);
+        });
+        pane.setOnMouseReleased(e -> mouse.set(e));
+
+        pane.setOnScroll(e -> {
+            int zoomLevel = mvp.get().zoomLevel();
+            int zoomDiff = (int) Math.round(e.getDeltaY() / 25);
+            int newZoomLevel = Math2.clamp(8, mvp.get().zoomLevel() + zoomDiff, 19);
+
+            double newMouseX = Math.scalb(e.getX() + mvp.get().xTopLeft(), -zoomLevel + newZoomLevel);
+            double newMouseY = Math.scalb(e.getY() + mvp.get().yTopLeft(), -zoomLevel + newZoomLevel);
+
+            double newXTopLeft = newMouseX - e.getX();
+            double newYTopLeft = newMouseY - e.getY();
+
+            mvp.set(new MapViewParameters(newZoomLevel,
+                    newXTopLeft,
+                    newYTopLeft));
+        });
     }
 }
