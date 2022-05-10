8d5b1af8c7639e7f763c5e1bec80511d9b0fef86
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-05-10 09:57:58 +0200

    Etape 9 moitié finie.

---
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -0,0 +1,113 @@
+package ch.epfl.javelo.gui;
+
+import ch.epfl.javelo.routing.*;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public final class RouteBean {
+
+    private ObservableList<Waypoint> waypoints;
+    private ObjectProperty<Route> route;
+    private ObjectProperty<ElevationProfile> elevationProfile;
+    private DoubleProperty highlightedPosition;
+    private RouteComputer routeComputer;
+    private Map<Pair<Integer, Integer>, Route> memoryCache;
+
+    private static final int MAX_STEP_LENGTH = 5;
+    private static final int CACHE_SIZE = 10;
+
+    public RouteBean(RouteComputer routeComputer) {
+        this.routeComputer = routeComputer;
+        waypoints = FXCollections.observableArrayList();
+        memoryCache = new LinkedHashMap<>(CACHE_SIZE, 0.75F, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CACHE_SIZE;
+            }
+        };
+
+        waypoints.addListener((ListChangeListener<? super Waypoint>) e -> buildRoute());
+
+        route.addListener((p, o, n) -> buildElevationProfile());
+    }
+
+    public void addWaypoint(Waypoint w) {
+        boolean b = waypoints.contains(w) ? waypoints.remove(w) : waypoints.add(w);
+    }
+
+    private void buildElevationProfile() {
+        if (route.get() == null) {
+            elevationProfile.set(null);
+        } else {
+            elevationProfile.set(
+                    ElevationProfileComputer.elevationProfile(
+                            route.get(),
+                            MAX_STEP_LENGTH));
+        }
+    }
+
+    private void buildRoute() {
+        if (waypoints.size() >= 2) {
+            List<Route> singleRoutes = new ArrayList<>();
+            for (int i = 0; i < waypoints.size() - 1; i++) {
+                int startNodeId = waypoints.get(i).closestNodeId();
+                int endNodeId = waypoints.get(i + 1).closestNodeId();
+                Pair<Integer, Integer> pair = new Pair<>(startNodeId, endNodeId);
+                boolean needToCalculateRoute = true;
+
+                for (Pair p : memoryCache.keySet()) {
+                    if (p.equals(pair)) {
+                        singleRoutes.add(memoryCache.get(p));
+                        needToCalculateRoute = false;
+                        break;
+                    }
+                }
+                if (needToCalculateRoute) {
+                    Route singleRoute = routeComputer.bestRouteBetween(startNodeId, endNodeId);
+                    if (singleRoute == null) {
+                        route.set(null);
+                        return;
+                    }
+                    memoryCache.put(pair, singleRoute);
+                    singleRoutes.add(singleRoute);
+                }
+            }
+            route.set(new MultiRoute(singleRoutes));
+        } else {
+            route.set(null);
+        }
+    }
+
+    public void setHighlightedPosition(DoubleProperty newHighlightedPosition) {
+        highlightedPosition = newHighlightedPosition;
+    }
+
+    public void setWaypoints(List<Waypoint> waypoints) {
+        this.waypoints = FXCollections.observableArrayList(waypoints);
+    }
+
+    public DoubleProperty highlightedPositionProperty() {
+        return highlightedPosition;
+    }
+
+    public double highlightedPosition() {
+        return highlightedPosition.doubleValue();
+    }
+
+    public ReadOnlyObjectProperty<ElevationProfile> getElevationProfile() {
+        return elevationProfile;
+    }
+
+    public ReadOnlyObjectProperty<Route> getRoute() {
+        return route;
+    }
+}
